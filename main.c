fc88075226f31b114d8caa2539d23ac170e39f09
Sh4vka <matvey11334422@gmail.com>
2025-05-05 17:31:23 +0300

    Работает

--- main.c
+++ main.c
@@ -11,14 +11,14 @@ Point snake[100];
 int lenght = 2;
 int dir = KEY_RIGHT;
 Point food;
-Point field[2] = { {50, 5}, {150, 30} }; 
+Point field[2] = { {50, 3}, {150, 40} }; 
 
 void drow_field() {
-        for (int i = field[0].x; i < field[1].x; i++) {
+        for (int i = field[0].x; i <= field[1].x; i++) {
                 mvprintw(field[0].y, i, "-");
                 mvprintw(field[1].y, i, "-");
         }
-        for (int i = field[0].y; i < field[1].y; i++) {
+        for (int i = field[0].y; i <= field[1].y; i++) {
                 mvprintw(i, field[0].x, "|");
                 mvprintw(i, field[1].x, "|");
         }
@@ -27,8 +27,13 @@ void drow_field() {
 void init_game() {
         for (int i = 0; i < lenght; i++) {
                 snake[i].x = 53;
-                snake[i].y = 25;
+                snake[i].y = 20;
         }
+        int width = field[1].x - field[0].x - 1;
+        int height = field[1].y - field[0].y - 1;
+
+        food.x = field[0].x + 1 + rand() % width;
+        food.y = field[0].y + 1 + rand() % height;
 }
 
 void draw_game() {
@@ -37,6 +42,7 @@ void draw_game() {
         for (int i = 0; i < lenght; i++) {
                 mvprintw(snake[i].y, snake[i].x, "O");
         }
+        mvprintw(food.y, food.x, "@");
         refresh();
 }
 
@@ -58,6 +64,24 @@ void update() {
                         snake[0].x--;
                         break;
         }
+
+        if (snake[0].x == food.x && snake[0].y == food.y) {
+                lenght++;
+                int width = field[1].x - field[0].x - 1;
+                int height = field[1].y - field[0].y - 1;
+
+                food.x = field[0].x + 1 + rand() % width;
+                food.y = field[0].y + 1 + rand() % height;
+        }
+
+        if (snake[0].x == field[0].x 
+                || snake[0].x == field[1].x
+                || snake[0].y == field[0].y
+                || snake[0].y == field[1].y) {
+                endwin();
+                printf("GAME OVER!\n");
+                exit(0);
+        }
 }
 
 int main() {
@@ -69,8 +93,12 @@ int main() {
 
         init_game();
         while(1) {
+                int old_ch = dir;
                 int ch = getch();
-                if (ch == KEY_UP || ch == KEY_DOWN || ch == KEY_RIGHT || ch == KEY_LEFT)
+                if ((ch == KEY_UP && old_ch != KEY_DOWN) 
+                        || (ch == KEY_DOWN && old_ch != KEY_UP) 
+                        || (ch == KEY_RIGHT && old_ch != KEY_LEFT)
+                        || (ch == KEY_LEFT && old_ch != KEY_RIGHT))
                         dir = ch;
                 update();
                 draw_game();
